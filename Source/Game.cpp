a2e1832b308b336f96992984001e8d02c84013c9
Vladimir Casa <viperezf@upbc.edu.mx>
2014-09-20 23:54:11 -0700

    Actividad 2: Mal Asigna el estado inicial, mira tu diagrama.

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -4,7 +4,7 @@
 
 CGame::CGame()
 {
-	estado == 0;
+	estado == 0;///ACT2: Mal, no debes de hacer una comparacion. Debes asigar el estado iniciar a tu juego.
 }
 
 // Con esta función eliminaremos todos los elementos en pantalla
